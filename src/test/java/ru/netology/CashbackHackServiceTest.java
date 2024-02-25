f6d1ab821e16dd133f5a0c9fbe3e0b94d0818f2a
Alexander Bortsov <A.bortsovv@yandex.ru>
2024-02-25 16:28:02 +0300

    junit4 tests changed

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @org.junit.Test
@@ -21,6 +21,6 @@ public class CashbackHackServiceTest {
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
